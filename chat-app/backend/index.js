bc8b2c5a3112f964d2118796af00aea623bfd64d
Delvin <delvin@ngauv.ch>
2023-11-22 18:58:04 +0100

    finished authentication with chatengine

--- chat-app/backend/index.js
+++ chat-app/backend/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const axios = require("axios");
 
 const app = express();
 app.use(express.json());
@@ -7,7 +8,20 @@ app.use(cors({ origin: true }));
 
 app.post("/authenticate", async (req, res) => {
   const { username } = req.body;
-  return res.json({ username: username, secret: "sha256..." });
+
+  try {
+    const userAPI = await axios.put(
+      "https://api.chatengine.io/users/",
+      { username: username, secret: username, first_name: username },
+      { headers: { "private-key": "2f74762b-1933-4a04-aed8-f8c33cb73baf" }}
+    );
+
+    return res.status(userAPI.status).json(userAPI.data);
+  } catch (error) {
+    return res.status(error.response.status).json(error.response.data);
+  }
 });
 
-app.listen(3001);
+app.listen(3001, () => {
+  console.log("Server is running on port 3001");
+});
