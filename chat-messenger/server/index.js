b3be029dac466c8fbb5b1ec061cccae2629225d0
Delvin <delvin@ngauv.ch>
2023-11-24 01:55:25 +0100

    working version

--- chat-messenger/server/index.js
+++ chat-messenger/server/index.js
@@ -7,8 +7,9 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 
 mongoose
-    .connect(
-        "mongodb+srv://delvin:1234@chat-messenger.vgdrasi.mongodb.net/Chat-Messenger?retryWrites=true&w=majority",
-         {useNewUrlParser: true, useUnifiedTopology: true}
-    )
-    .then(() => app.listen(5001, () => console.log("Listening")));
+  .connect(
+    "mongodb+srv://delvin:1234@chat-messenger.vgdrasi.mongodb.net/Chat-Messenger?retryWrites=true&w=majority",
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => app.listen(5001, () => console.log("Listening")))
+  .catch((error) => console.log(error));
